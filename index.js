e953bbb110d0d996f745ddbba6347102cec1b15b
hrsgmt <hrsgmt@gmail.com>
2026-02-06 15:18:14 +0530

    Fix Render entrypoint (root index.js)

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-import Fastify from "fastify";
-
-const app = Fastify();
-
-app.get("/", async () => {
-  return { ok: true };
-});
-
-const PORT = process.env.PORT || 4000;
-
-app.listen({ port: PORT, host: "0.0.0.0" })
-  .then(() => console.log("Server running on", PORT))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+import "./src/server.js";
